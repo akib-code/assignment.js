4b38b1ca5b0cf44f8ffa73397aa26c4ba7af8185
nesarprogrammer <akib6818@gmail.com>
2021-02-10 23:22:04 +0600

    finished work

---
+++ assignment.js
@@ -0,0 +1,21 @@
+function kilometerToMeter(kilometer){
+    var meter = kilometer / 3;
+    return meter;
+}
+
+function budgetCalculator(watch, mobile, laptop){
+    var watchcount = watch  * 6;
+    var mobilecount = mobile * 6;
+    var laptopcount = laptop * 6;
+    var totalbudget = watchcount + mobilecount + laptopcount;
+    return totalbudget;
+}
+
+function hotelCost(days){
+    var dayscount = 10;
+    return totalhotelcost;
+}
+
+function megaFriend(friend){
+    var
+}
